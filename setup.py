b29684ed3e348fb995cc4e154be57ef5bd64e415
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2018-04-15 10:55:37 -0700

    added zip_safe

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ setup(
     name='photogal',
     packages=find_packages(),
     include_package_data=True,
+    zip_safe=False,
     install_requires=[
-        'flask',
+        'Flask',
         'Pillow',
         'SQLAlchemy'
     ],
