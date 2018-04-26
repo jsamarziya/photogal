57f6443d11b3291e4444fcd77e319f042656350e
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2018-04-25 19:44:20 -0700

    added CreateGallery mutation

--- src/photogal/schema.py
+++ src/photogal/schema.py
@@ -18,6 +18,7 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+from photogal.database import db
 from photogal.model import Gallery as GalleryModel, Image as ImageModel
 
 
@@ -33,6 +34,42 @@ class Image(SQLAlchemyObjectType):
         interfaces = (relay.Node,)
 
 
+class CreateGallery(graphene.Mutation):
+    class Arguments:
+        created = graphene.DateTime(required=False)
+        last_modified = graphene.DateTime(required=False)
+        name = graphene.String(required=False)
+        description = graphene.String(required=False)
+        order_index = graphene.Int(required=False)
+        public = graphene.Boolean(required=False)
+        gallery_image_id = graphene.Int(required=False)
+
+    ok = graphene.Boolean()
+    gallery = graphene.Field(lambda: Gallery)
+
+    # noinspection PyMethodMayBeStatic,PyUnusedLocal
+    def mutate(self, info,
+               created=None,
+               last_modified=None,
+               name=None,
+               description=None,
+               order_index=None,
+               public=None,
+               gallery_image_id=None):
+        gallery = GalleryModel(
+            created=created,
+            last_modified=last_modified,
+            name=name,
+            description=description,
+            order_index=order_index,
+            public=public,
+            gallery_image_id=gallery_image_id
+        )
+        db.session.add(gallery)
+        db.session.commit()
+        return CreateGallery(gallery=gallery)
+
+
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
     gallery = graphene.Field(Gallery, id=graphene.Int())
@@ -40,17 +77,17 @@ class Query(graphene.ObjectType):
     galleries = SQLAlchemyConnectionField(Gallery)
     images = SQLAlchemyConnectionField(Image)
 
-    # noinspection PyMethodMayBeStatic
-    def resolve_gallery(self, info, **args):
-        query = Gallery.get_query(info)
-        gallery_id = args.get('id')
-        return query.filter(GalleryModel.id == gallery_id).first()
+    # noinspection PyMethodMayBeStatic,PyShadowingBuiltins,PyUnusedLocal
+    def resolve_gallery(self, info, id=None):
+        return GalleryModel.query.filter(GalleryModel.id == id).first()
 
-    # noinspection PyMethodMayBeStatic
-    def resolve_image(self, info, **args):
-        query = Image.get_query(info)
-        image_id = args.get('id')
-        return query.filter(ImageModel.id == image_id).first()
+    # noinspection PyMethodMayBeStatic,PyShadowingBuiltins,PyUnusedLocal
+    def resolve_image(self, info, id=None):
+        return ImageModel.query.filter(ImageModel.id == id).first()
 
 
-schema = graphene.Schema(query=Query, types=[Gallery, Image])
+class Mutations(graphene.ObjectType):
+    create_gallery = CreateGallery.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutations)
