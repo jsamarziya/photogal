347007c6da46688da2091027bfed5f66f9f7c5c6
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2018-05-09 22:32:36 -0700

    added docstring

--- tests/config.py
+++ tests/config.py
@@ -15,5 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Photogal configuration values for testing."""
+
 SQLALCHEMY_DATABASE_URI = 'sqlite://'
 TESTING = True
